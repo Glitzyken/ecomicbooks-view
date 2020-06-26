8c92ada7e6d0af2957babf9838e9b91183460815
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-06-26 18:29:34 +0100

    reset css and convert lang to scss

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <div class="sample"></div>
+    <NavBar />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
+    NavBar
   },
 
   data: () => ({
@@ -54,3 +20,12 @@ export default {
   })
 };
 </script>
+
+<style lang="scss">
+@import './assets/sass/_variables.scss';
+
+.sample {
+  background: $bg;
+  height: 400px;
+}
+</style>
