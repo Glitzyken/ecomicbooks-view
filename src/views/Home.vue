e4b388b982e7f43c1d5e476985364f884b94ea1e
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-06-27 21:50:33 +0100

    resolve dummy data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,17 @@
     <h1><span class="custom-span"> New </span> Issues</h1>
 
     <v-row>
-      <v-col cols="6">
+      <v-col cols="6" v-for="issue in issues" :key="issue.imgUrl">
         <div class="card">
           <v-img
             class="rounded"
             contain
             max-width="150px"
-            src="../assets/img/sample-1.jpg"
+            :src="issue.imgUrl"
             lazy-src="../assets/img/lazy-loader.jpg"
-            alt="issue image"
+            :alt="issue.title"
           />
-          <div class="card__text">Black Panther 2020 Issue #1</div>
+          <div class="card__text">{{ issue.title }}</div>
         </div>
       </v-col>
     </v-row>
@@ -21,8 +21,60 @@
 </template>
 
 <script>
+const img1 = require('../assets/img/sample-1.jpg');
+const img2 = require('../assets/img/sample-2.jpg');
+const img3 = require('../assets/img/sample-3.jpg');
+const img4 = require('../assets/img/sample-4.jpg');
+const img5 = require('../assets/img/sample-5.jpg');
+const img6 = require('../assets/img/sample-6.jpg');
+const img7 = require('../assets/img/sample-7.jpg');
+const img8 = require('../assets/img/sample-8.jpg');
+const img9 = require('../assets/img/sample-9.jpg');
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      issues: [
+        {
+          imgUrl: img1,
+          title: 'Black Panther Issue #22'
+        },
+        {
+          imgUrl: img2,
+          title: 'Rescue 2020 Issue #4'
+        },
+        {
+          imgUrl: img3,
+          title: 'Die Issue #1'
+        },
+        {
+          imgUrl: img4,
+          title: 'Thor Issue #8'
+        },
+        {
+          imgUrl: img5,
+          title: 'Gung Ho Issue #3'
+        },
+        {
+          imgUrl: img6,
+          title: 'Firefly Issue #1'
+        },
+        {
+          imgUrl: img7,
+          title: 'Tartarus Issue #9'
+        },
+        {
+          imgUrl: img8,
+          title: 'Transformers Issue #6'
+        },
+        {
+          imgUrl: img9,
+          title: 'The Low, Low Woods Issue 34'
+        }
+      ]
+    };
+  },
   components: {}
 };
 </script>
